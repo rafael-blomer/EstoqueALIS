12464fc43bb4da278565cdbd44a72ade7c0c3f64
rafael-blomer <rafael.blomer@gmail.com>
2025-08-13 14:13:37 -0300

    projeto criado

---
+++ EstoqueALIS/src/main/java/br/com/rafaelblomer/EstoqueAlisApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelblomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueAlisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueAlisApplication.class, args);
+	}
+
+}
